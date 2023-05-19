dd843fa5547ce7ef82041010bc53607b8d2488c0
diegomm6 <dgo.26.62@gmail.com>
2023-05-19 21:17:32 +0900

    more experiments added

--- testNetsizeCR.py
+++ testNetsizeCR.py
@@ -14,19 +14,12 @@ from simulationCR import SimulationCR
 # sinlge family, using parallel computation
 
 def get_family():
-    d = 8
-    q = 31
-    maxfreq = 280
-    mingap = 8
-    liFanGenerator = LiFanFamily(q=q, maxfreq=maxfreq, mingap=mingap)
-    liFan_fam1 = liFanGenerator.get_3l_family(277, d)
-    liFan_fam2 = liFanGenerator.get_3l_family(281, d)
-    liFan_fam3 = liFanGenerator.get_3l_family(283, d)
-    liFan_fam4 = liFanGenerator.get_3l_family(287, d)
+    q=31
+    lr_fhssGenerator = LR_FHSS_DriverFamily(q)
+    lr_fhss_family = lr_fhssGenerator.get_lr_fhss_family()
+    lr_fhss_family = lr_fhss_family *8
 
-    liFan_fam5 = np.concatenate((liFan_fam1, liFan_fam2, liFan_fam3, liFan_fam4))
-
-    return liFan_fam5
+    return lr_fhss_family
 
 
 # test a single method for several number of nodes
@@ -39,7 +32,7 @@ def get_avg_packet_collision_rate(v):
     seq_length = 31
     startLimit = 500
     CR = 2
-    useGrid = False
+    useGrid = True
     family = get_family()
 
     nodes = int(v)
@@ -58,9 +51,15 @@ if __name__ == "__main__":
     netSizesCR2 = [1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3, 8e3, 9e3, 1e4, 1.2e4,
                    1.4e4, 1.6e4, 1.8e4, 2e4, 2.33e4, 2.66e4, 3e4, 5e4, 1e5]
     
-    netSizes = netSizesCR2
 
-    print('CR = 2; liFan_fam5')
+    lifan_netSizes = [1e2, 2e2, 3e2, 4e2, 5e2, 6e2, 7e2, 8e2, 9e2, 1e3,
+                         1.2e3, 1.4e3, 1.6e3, 1.8e3, 2e3, 2.25e3, 2.5e3,
+                         2.75e3, 3e3, 3.33e3, 3.66e3, 4e3, 4.33e3, 4.66e3,
+                         5e3, 5.5e3, 6e3, 6.5e3, 7e3, 7.5e3, 8e3, 9e3, 1e4]
+    
+    netSizes = lifan_netSizes
+
+    print('CR = 2; lr_fhss_family')
 
     pool = Pool(processes = len(netSizes))
     result = pool.map(get_avg_packet_collision_rate, netSizes)
